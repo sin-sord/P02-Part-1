436143d920e2a10d17f3b0b39ddb46a10d65059a
DESKTOP-RQH4JFP\Zalya <zalya@sheridancollege.ca>
2024-01-28 16:15:48 -0500

    Feature 2 - Gate Trigger complete: Player can touch a switch to open and close the gate.

---
+++ Assets/Assignment/Script/GateSwitch.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateSwitch : MonoBehaviour
+{
+    public GameObject Gate;
+    public GameObject Switch;
+    SpriteRenderer spriteRenderer;
+    public Transform SwitchSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(Gate.activeInHierarchy)
+        {
+            Gate.SetActive(false);
+            spriteRenderer.color = Color.red;
+        } else
+        {
+            Gate.SetActive(true);
+            spriteRenderer.color= Color.green;
+
+        }
+    }
+}
